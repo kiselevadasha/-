03467620d5e696a25142a27673bd55282fa6750e
ксилевдаша <146414298+kiselevadasha@users.noreply.github.com>
2024-01-22 16:22:09 +0500

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, s;char d;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите действие");
+            d = Convert.ToChar(Console.ReadLine());
+            switch (d)
+            {
+                case '+': s = a + b; Console.WriteLine("Сумма = "+s); break;
+                case '-': s = a - b; Console.WriteLine("Разность = "+s); break;
+                case '*': s = a * b; Console.WriteLine("Произведение = "+s); break;
+                case '/': s = a / b; Console.WriteLine("Частное = "+s); break;
+                    default: Console.WriteLine("Нет такого действия"); break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
